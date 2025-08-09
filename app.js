91be78dec67e60332e660c18f1e361df8f3645af
kev613 <kevmeza8@gmail.com>
2025-08-08 22:02:40 -0600

    subidaarchivos
    subidaarchivos

---
+++ app.js
@@ -0,0 +1,7 @@
+function sumar() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let resultado = num1 + num2;
+    alert("La suma es: " + resultado);
+}
+
